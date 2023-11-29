eba7411051442eeffe3789109d3676e25252f90e
ARCtechmo <arctitan78@gmail.com>
2023-11-29 11:06:46 -0500

    first hello world programs in c along with executable, pre-processing, and assembly files

---
+++ hello.c
@@ -0,0 +1,8 @@
+/* a simple program to print 'Hello world!*/
+/* to run the program use this command: gcc -o hello hello.c */
+/* it is also best to use the -Wall option to display all compiler warning messages*/
+#include <stdio.h>
+int main(){
+    printf("Hello world!\n");
+    return 0;
+}
---
+++ helloworld.c
@@ -0,0 +1,8 @@
+/* a simiple hello world program*/
+/* to run the program use this command: gcc -o hello hello.c */
+/* it is also best to use the -Wall option to display all compiler warning messages*/
+#include <stdio.h>
+int main(){
+    printf("Hello world!\n");
+    return 0;
+}
